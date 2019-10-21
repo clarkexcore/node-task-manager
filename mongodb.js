bdca65b2ca1f6c808e2101fe0363175027996f0d
Alex Clarke <clarkexcore@gmail.com>
2019-10-21 17:57:47 -0400

    Mongo DB Stuff

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,12 @@
 //CRUD Operations
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if(error){
