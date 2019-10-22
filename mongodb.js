a65a76a5328e5bf2f7c608d7095bcd15f675d97f
Alex Clarke <clarkexcore@gmail.com>
2019-10-22 00:15:47 -0400

    Basic CRUD done. Left with Delete

--- mongodb.js
+++ mongodb.js
@@ -5,61 +5,40 @@ const {MongoClient, ObjectID} = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
 
-const id = new ObjectID();
-console.log(id);
-
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
         return console.log("Unable to connect to Database");
     }
 
     const db = client.db(database)
 
-    // db.collection('users').insertOne({
-    //     name: "Alex",
-    //     age: 29
-    // }, (error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert user.");
+    // db.collection('users').updateOne({ 
+    //     _id: new ObjectID("5dae1d8591b8370dc3bd26c8")
+    // }, {
+    //     $inc: {
+    //         age: 2
     //     }
-
-    //     console.log(result.ops);
-    // });
-    // db.collection('users').insertMany([
-    //     {
-    //         name: "Matty",
-    //         age: 28
-    //     }, {
-    //         name: "Tnerb",
-    //         age: 29
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert more users");
-    //     }
-
-    //     console.log(result.ops);
+    // }).then((res) => {
+    //     console.log(res);
+    // }).catch((err) => {
+    //     console.log(err);
     // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         details: "Clean the car",
-    //         completed: true
-    //     }, {
-    //         details: "Make sure that the Tours page is done on MRG Concerts",
-    //         completed: false
-    //     }, {
-    //         details: "Go out to Vote",
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert tasks into database");
-    //     }
-
-    //     console.log(result.ops);
+    // db.collection("users").deleteMany({
+    //     age: 25
+    // }).then((res) => {
+    //     console.log(res);
+    // }).catch((err) => {
+    //     console.log(err);
     // })
 
+    db.collection("tasks").deleteOne({
+        details: "Clean the car"
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    })
 })
 
 
